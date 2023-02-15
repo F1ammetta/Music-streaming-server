ff1452438d4c85e0217d9a9af6e75dc66930f28e
Sergio <sdepineresramirez@utb.edu.co>
2023-02-14 22:47:11 -0500

    migrated https and  express

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
-import { where } from "sequelize";
-
-const {createServer} = require('http');
-const {stat, createReadStream, createWriteStream} = require('fs');
-const {promisify} = require('util');
-const fileInfo = promisify(stat);
-const sequelize = require('./db.cjs');
+var express = require('express');
+var app = express();
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+var mm = require('music-metadata');
+var util = require('util');
+var { createReadStream, stat } = require('fs');
+var { promisify } = require('util');
+var fileInfo = promisify(stat);
 const Song = require('./song.cjs');
-const mm = require('music-metadata');
-const util = require('util');
+const sequelize = require('./db.cjs');
+var { createServer } = require('http');
 let Readable = require('stream').Readable; 
 
 sequelize.sync().then(() => console.log('db is ready'));
-
-const port: number = 3334;
-let folder: String = '/home/sergiodkpo/Music/'; // 'D:\\Users\\Sergio\\Music\\Actual Music\\'
+const port: number = 443;
+let folder: String = 'D:\\Users\\Sergio\\Music\\Actual Music\\'
+// '/home/sergiodkpo/Music/';
 let filepath: String;
 
-// TODO:add certificates for https
-
+var options = {
+    key: fs.readFileSync('C:\\Certbot\\live\\kwak.sytes.net\\privkey.pem'),
+    cert: fs.readFileSync('C:\\Certbot\\live\\kwak.sytes.net\\fullchain.pem'),
+};
 
 function notFound(res) {
     res.writeHead(404, {'Content-Type': 'text/html'});
@@ -83,132 +88,118 @@ function handleAll(res, metasongs) {
     res.end(string_meta);
 }
 
-createServer(async (req, res) => {
 
-    if (req.url.split('/')[1] == 'tracks') {
+app.get('/tracks/:id', async (req, res) => {
+    try {
+        let song = await Song.findOne({where: {id: req.params.id}})
+        filepath = `${folder}${song.filename}`;
+        let {size} = await fileInfo(filepath); // let range = req.headers.range;
+        streamSong(res, size);
+    }
+    catch(e) {
+        notFound(res);
+    }
+});
+
+app.get('/v0/album/all', async (req, res) => {
+    let songs = await Song.findAll();
+    let albums = [];
+    for (let i = 0; i < songs.length; i++) {
         try {
-            let song = await Song.findOne({where: {id: req.url.split('/')[2]}})
-            filepath = `${folder}${song.filename}`;
-            let {size} = await fileInfo(filepath); // let range = req.headers.range;
-            streamSong(res, size);
-        }
-        catch(e) {
+            filepath = `${folder}${songs[i].filename}`;
+            let metadata = await mm.parseFile(filepath);
+            if (!albums.some(album => album.title == metadata.common.album)) {
+                let album = {title: metadata.common.album, artist: metadata.common.artist, id: i+1};
+                albums.push(album);
+            }
+        } catch (e){
             notFound(res);
         }
     }
-    else if (req.url.split('/')[1] == 'v0') {
-        let id = req.url.split('/')[2];
-        if (id == 'album') {
-            id = req.url.split('/')[3];
-            if (id == 'all'){
-                let songs = await Song.findAll();
-                let albums = [];
-                for (let i = 0; i < songs.length; i++) {
-                    try {
-                        filepath = `${folder}${songs[i].filename}`;
-                        let metadata = await mm.parseFile(filepath);
-                        if (!albums.some(album => album.title == metadata.common.album)) {
-                            let album = {title: metadata.common.album, artist: metadata.common.artist, id: i+1};
-                            albums.push(album);
-                        }
-                    } catch (e){
-                        notFound(res);
-                    }
-                }
 
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain; charset=utf8'
-                });
-                let string_meta: String = JSON.stringify(albums);
-                res.end(string_meta);
-            }
-            else if (id == 'cover') {
-                let id = req.url.split('/')[4];
-                try {
-                    let song = await Song.findOne({where: {id: id}});
-                    filepath = `${folder}${song.filename}`;
-                    let metadata = await mm.parseFile(filepath);
-                    metadata.common.id = song.id;
-                    handleCover(res, metadata);
-                }
-                catch(e) {
-                    notFound(res);
-                }
-            }
-            else {
-                id = id;
-                let song = await Song.findOne({where: {id: id}});
-                let songs = await Song.findAll({where: {album: song.album}});
-                let albumsongs = [];
-                for (let i = 0; i < songs.length; i++) {
-                    try {
-                        filepath = `${folder}${songs[i].filename}`;
-                        let metadata = await mm.parseFile(filepath);
-                        metadata.common.id = songs[i].id;
-                        metadata.common.duration = metadata.format.duration;
-                        delete metadata.common.picture;
-                        albumsongs.push(metadata.common);
-                    } catch (e) {
-                        notFound(res);
-                    }
-                    // if (metadata.common.album == id) {
-                    //     albumsongs.push(songs[i]);
-                    // }
-                }
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf8'
+    });
+    let string_meta: String = JSON.stringify(albums);
+    res.end(string_meta);
+});
 
-                res.writeHead(200, {
-                    'Content-Type': 'text/plain; charset=utf8'
-                });
-                let string_meta: String = JSON.stringify(albumsongs);
-                res.end(string_meta);
-            }
-        }
-        if (id == 'cover'){
-            id = req.url.split('/')[3];
-            try {
-                let song = await Song.findOne({where: {id: id}});
-                filepath = `${folder}${song.filename}`;
-                let metadata = await mm.parseFile(filepath);
-                metadata.common.id = song.id;
-                handleCover(res, metadata);
-            }
-            catch (error) {
-                notFound(res);
-            }
-        }
-        else if (id == 'all') {
-            let metasongs = [];
-            let songs = await Song.findAll();
-            
-            for (let i = 0; i < songs.length; i++) {
-                try{
-                    filepath = `${folder}${songs[i].dataValues.filename}`;
-                    let metadata = await mm.parseFile(filepath);
-                    delete metadata.common.picture;
-                    metadata.common.duration = metadata.format.duration;
-                    metadata.common.id = songs[i].dataValues.id;
-                    metasongs.push(metadata.common);
-                } catch (e) {
-                    notFound(res);
-                }
-            }
-            
-            handleAll(res, metasongs);
-            
-        }
-        else {
-            try {
-                let song = await Song.findOne({where: {id: id}});
-                filepath = `${folder}${song.filename}`;
-                let metadata = await mm.parseFile(filepath);
-                handleMeta(res, metadata);
-            }
-            catch (error) {
-                notFound(res);   
-            }
+app.get('/v0/album/:id', async (req, res) => {
+    let id = req.params.id;
+    let song = await Song.findOne({where: {id: id}});
+    let songs = await Song.findAll({where: {album: song.album}});
+    let albumsongs = [];
+    for (let i = 0; i < songs.length; i++) {
+        try {
+            filepath = `${folder}${songs[i].filename}`;
+            let metadata = await mm.parseFile(filepath);
+            metadata.common.id = songs[i].id;
+            metadata.common.duration = metadata.format.duration;
+            delete metadata.common.picture;
+            albumsongs.push(metadata.common);
+        } catch (e) {
+            notFound(res);
         }
     }
-    else{
-        notFound(res);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf8'
+    });
+    let album = {
+        title: albumsongs[0].album,
+        songs: albumsongs
     }
-}).listen(port, () => console.log('Running on port: ', port));
+    let string_meta: String = JSON.stringify(album);
+    res.end(string_meta);
+});
+
+
+app.get('/v0/cover/:id', async (req, res) => {
+    let id = req.params.id;
+    try {
+        let song = await Song.findOne({where: {id: id}});
+        filepath = `${folder}${song.filename}`;
+        let metadata = await mm.parseFile(filepath);
+        metadata.common.id = song.id;
+        handleCover(res, metadata);
+    }
+    catch (error) {
+
+    }
+});
+
+app.get('/v0/all', async (req, res) => {
+    let metasongs = [];
+    let songs = await Song.findAll();
+    
+    for (let i = 0; i < songs.length; i++) {
+        try{
+            filepath = `${folder}${songs[i].dataValues.filename}`;
+            let metadata = await mm.parseFile(filepath);
+            delete metadata.common.picture;
+            metadata.common.duration = metadata.format.duration;
+            metadata.common.id = songs[i].dataValues.id;
+            metasongs.push(metadata.common);
+        } catch (e) {
+            notFound(res);
+        }
+    }
+    
+    handleAll(res, metasongs);
+    
+});
+
+app.get('/v0/meta/:id', async (req, res) => {
+    try {
+        let song = await Song.findOne({where: {id: req.params.id}});
+        filepath = `${folder}${song.filename}`;
+        let metadata = await mm.parseFile(filepath);
+        handleMeta(res, metadata);
+    }
+    catch (error) {
+        notFound(res);   
+    }
+});
+
+https.createServer(options, app).listen(port, () => console.log('Running on port: ', port));
+    
